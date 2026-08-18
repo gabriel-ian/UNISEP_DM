9b1ec8b5ed9fca45e6af90ac8702e886e337a638
gabriel-ian <gabrielcenci43@gmail.com>
2026-08-18 19:45:38 -0300

    conceitos básicos de Javascript

---
+++ js/index.js
@@ -0,0 +1,27 @@
+//console.log("Olá Mundo!");
+
+//function teste(){
+//    console.log("Tchau Mundo!");
+//}
+
+//teste();
+
+//var gabriel = "Gabriel boa Noite!";
+
+//console.log(gabriel);
+
+var teste = "100";
+var a = "100";
+var b = 100;
+
+console.log(typeof teste)  // 
+console.log(Number(a) + b);
+
+var c = "AAAAAA";
+var d = 10;
+
+console.log(Number(c) + d);
+
+var testee;
+
+console.log(typeof testee);
